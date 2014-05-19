209141a8c00a1aafe448dcefc5a7525cf2489a36
peterqliu <peterqliu@gmail.com>
2014-05-19 11:01:19 -0700

    solved direction-flip bug

--- firebus-modified.js
+++ firebus-modified.js
@@ -43,18 +43,18 @@ function newBus(bus, firebaseId) {
 				var xcoordforlabel=$(this).attr('xcoord')+500;
 				var ycoordforlabel=$(this).attr('ycoord');	
 				var directionColor=$(this).attr('direction').indexOf('OB') > -1 ? outboundcolor : inboundcolor;	
+				var directiontag=$(this).attr('direction').indexOf('OB') > -1 ? ' outbound' : ' inbound';
 
 				d3.json('stops.refactored.v3.json',function(json){
+
 					var busname=json[bus.routeTag+direction]['Name'];							
 					var destination= function() {
 						if ($('.zoomed').length==0)
-							{return 'Click to inspect this line'}
+							{return 'Click to inspect '}
 						else 
 							{return json[fudge(bus.routeTag+direction)]['Title'];}
 						}
-
-
-					makelabel(xcoordforlabel+'500',ycoordforlabel,busname,directionColor,destination);			
+					makelabel(xcoordforlabel+'500',ycoordforlabel,busname,directionColor,destination,directiontag);			
 				})
 
 			if ($('.zoomed').length==0)	
@@ -67,19 +67,14 @@ function newBus(bus, firebaseId) {
 		.on('mouseleave',function(){removelabel();
 			$('.viewport').attr('highlighting','no');
 		})
-		//.on('mouseout',function(){removelabel()})
-
 		.on('click',function() {
 			var xcoordforlabel=d3.select(this).attr('xcoord');
 			var ycoordforlabel=d3.select(this).attr('ycoord');
 			zoomto(xcoordforlabel,ycoordforlabel);
 
-
 			showbusesonline(bus.routeTag,direction);
 			showroutestops(bus.routeTag+direction,directionColor,bus.vtype);			
 		})
-
-
 		.append('g')
 		.attr('class','grower')    
 	    .append('path')
@@ -142,20 +137,14 @@ f.on("child_changed", function(s) {
     var direction= bus.dirTag && bus.dirTag.indexOf('OB') > -1 ? "_OB" : "_IB";
     var route=bus.routeTag;
 
-    //detect if bus changed lines
-    if(route!=d3.select('#bus'+busMarker).attr('route'))
-    	{console.log('a bus just changed their route line!');}
+    //detect if bus changed direction
+    if(direction!=d3.select('#bus'+busMarker).attr('direction'))
+    	{console.log('a bus just changed their direction!');
+    	d3.select('#bus'+busMarker)
+    	.on('click',null)
+    	.on('mouseover',null)
+    	.on('mouseleave',null)
 
-	 d3.select('#bus'+busMarker)
-	    .attr('style','-webkit-transform:translate('+xcoord+'px,'+ycoord+'px)')
-		.attr('xcoord',xcoord+20)
-		.attr('ycoord',ycoord-10)   
-		.attr('direction',direction)		
-		.attr('route',route)
-	    .select('path')
-	    .attr('transform','rotate('+s.val().heading+')')
-	    .attr('onclick','')
-		//.attr('onclick','showroutestops("'+route+direction+'"), showbusesonline("'+route+'", "'+direction+'")')	    
 		.on('click',function(){
 			var xcoordforlabel=d3.select(this).attr('xcoord');
 			var ycoordforlabel=d3.select(this).attr('ycoord');
@@ -165,7 +154,58 @@ f.on("child_changed", function(s) {
 
     		var directionColor = direction.indexOf('OB') > -1 ? outboundcolor : inboundcolor;			
 			showroutestops(bus.routeTag+direction,directionColor,bus.vtype);	
-		})			
+		})
+		.on('mouseover',function() {
+
+			//Fire only when the mouse isn't already held down (i.e. dragging)
+			if ($('body:active').length==0){			
+
+				$('.viewport').attr('highlighting','yes');			
+				var target=d3.select(this);
+				var xcoordforlabel=$(this).attr('xcoord')+500;
+				var ycoordforlabel=$(this).attr('ycoord');	
+				var directionColor=$(this).attr('direction').indexOf('OB') > -1 ? outboundcolor : inboundcolor;	
+				var directiontag=$(this).attr('direction').indexOf('OB') > -1 ? ' outbound' : ' inbound';
+
+				d3.json('stops.refactored.v3.json',function(json){
+					var busname=json[bus.routeTag+direction]['Name'];							
+					var destination= function() {
+						if ($('.zoomed').length==0)
+							{return 'Click to inspect '}
+						else 
+							{return json[fudge(bus.routeTag+direction)]['Title'];}
+						}
+
+
+					makelabel(xcoordforlabel+'500',ycoordforlabel,busname,directionColor,destination,directiontag);			
+				})
+
+			if ($('.zoomed').length==0)	
+				{
+				drawpath(bus.routeTag+direction,directionColor);
+				}
+
+			}
+		})
+		.on('mouseleave',function(){removelabel();
+			$('.viewport').attr('highlighting','no');
+		})
+		}
+		
+	$('#bus'+busMarker)
+	    .attr('style','-webkit-transform:translate('+xcoord+'px,'+ycoord+'px)')
+		.attr('xcoord',xcoord+20)
+		.attr('ycoord',ycoord-10)   
+		.attr('direction',direction)		
+		.attr('route',route)
+	    .select('path')
+	    .attr('transform','rotate('+s.val().heading+')')
+	    .attr('onclick','')
+
+
+
+
+
 	
 	countbuses();
 
